3145e9f587a60d2d79a25e576c86911abdb678ab
Adam Mitręga <amitrega01@gmail.com>
2017-03-01 19:07:38 +0100

    created new MenuScreen

--- core/src/com/amitrega01/snake/MyGame.java
+++ core/src/com/amitrega01/snake/MyGame.java
@@ -17,9 +17,6 @@ public class MyGame extends Game {
 	@Override
 	public void render () {
 		super.render();
-		//Gdx.gl.glClearColor(1, 0, 0, 1);
-		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
 	}
 	
 	@Override
---
+++ core/src/com/amitrega01/snake/Screens/MenuScreen.java
@@ -0,0 +1,56 @@
+package com.amitrega01.snake.Screens;
+
+import com.amitrega01.snake.MyGame;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Created by amitr on 01.03.2017.
+ */
+
+public class MenuScreen implements Screen {
+
+    private Game game;
+
+    public MenuScreen(MyGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
