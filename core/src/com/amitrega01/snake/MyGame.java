253dddd95de345647b2069b4c32c4e1b3b38c9f4
Adam Mitręga <amitrega01@gmail.com>
2017-03-01 19:15:24 +0100

    minro changes

--- core/src/com/amitrega01/snake/MyGame.java
+++ core/src/com/amitrega01/snake/MyGame.java
@@ -12,13 +12,12 @@ public class MyGame extends Game {
 	public static  int WIDTH = 450;
 	public static  int HEIGHT = 800;
 	static float scl2 =25;
-	static int scl = 25;
+	public static int scl = 25;
 	public static int fps = 8;
-	int col,row;
+	public static int col,row;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 		//scalling system
 		scl2 = scl*Gdx.graphics.getWidth() / WIDTH;
 		scl = Math.round(scl2);
@@ -28,6 +27,9 @@ public class MyGame extends Game {
 		HEIGHT = (Gdx.graphics.getHeight()/scl) *scl ;
 		col = WIDTH / scl;
 		row = HEIGHT /scl;
+		//end of scalling system
+
+		batch = new SpriteBatch();
 	}
 
 	@Override
