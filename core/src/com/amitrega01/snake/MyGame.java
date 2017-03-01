b2e30e1f5884f835cd8219b24d3150af335f147a
Adam Mitręga <amitrega01@gmail.com>
2017-03-01 19:10:46 +0100

    created scaling system

--- core/src/com/amitrega01/snake/MyGame.java
+++ core/src/com/amitrega01/snake/MyGame.java
@@ -9,9 +9,25 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGame extends Game {
 	public SpriteBatch batch;
 
+	public static  int WIDTH = 450;
+	public static  int HEIGHT = 800;
+	static float scl2 =25;
+	static int scl = 25;
+	public static int fps = 8;
+	int col,row;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		//scalling system
+		scl2 = scl*Gdx.graphics.getWidth() / WIDTH;
+		scl = Math.round(scl2);
+		System.out.println(scl2);
+		System.out.println(scl);
+		WIDTH = (Gdx.graphics.getWidth()/scl) *scl ;
+		HEIGHT = (Gdx.graphics.getHeight()/scl) *scl ;
+		col = WIDTH / scl;
+		row = HEIGHT /scl;
 	}
 
 	@Override
