c0448f3432bca1cbe1f316b3d2c494aefd6a9b0b
Adam Mitręga <amitrega01@gmail.com>
2017-03-01 18:56:14 +0100

    renaming main class

--- android/src/com/amitrega01/snake/AndroidLauncher.java
+++ android/src/com/amitrega01/snake/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.amitrega01.snake.Game;
+import com.amitrega01.snake.MyGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Game(), config);
+		initialize(new MyGame(), config);
 	}
 }
--- desktop/src/com/amitrega01/snake/desktop/DesktopLauncher.java
+++ desktop/src/com/amitrega01/snake/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.amitrega01.snake.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.amitrega01.snake.Game;
+import com.amitrega01.snake.MyGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new MyGame(), config);
 	}
 }
