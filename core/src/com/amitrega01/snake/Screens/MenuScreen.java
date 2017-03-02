db66cc4d117821e48057f86ada7d31bf716f81c3
Adam Mitręga <amitrega01@gmail.com>
2017-03-02 22:36:24 +0100

    changes in menu

--- core/src/com/amitrega01/snake/Screens/MenuScreen.java
+++ core/src/com/amitrega01/snake/Screens/MenuScreen.java
@@ -34,30 +34,60 @@ public class MenuScreen implements Screen {
     public MyGame game;
     private Skin skin;
     private Stage stage;
+
     public MenuScreen(final MyGame game) {
         this.game = game;
         skin = new Skin(Gdx.files.internal("quantum-horizon/skin/quantum-horizon-ui.json"));
         stage = new Stage();
-        int btnW = 10* scl;
-        int btnH = 3* scl;
+        int btnW = 10 * scl;
+        int btnH = 3 * scl;
 
-        final TextButton startBtn = new TextButton("Start",skin);
+        final TextButton startBtn = new TextButton("Start", skin);
         startBtn.setWidth(btnW);
         startBtn.setHeight(btnH);
-        startBtn.setPosition(WIDTH/2 -10* scl/2,HEIGHT/2);
+        startBtn.setPosition(WIDTH / 2 - 10 * scl / 2, HEIGHT / 2);
+
+        final TextButton optionBtn = new TextButton("Options", skin);
+        optionBtn.setWidth(btnW);
+        optionBtn.setHeight(btnH);
+        optionBtn.setPosition(WIDTH / 2 - 10 * scl / 2, HEIGHT / 2 - btnH - scl);
+
+        final TextButton exitBtn = new TextButton("Exit", skin);
+        exitBtn.setWidth(btnW);
+        exitBtn.setHeight(btnH);
+        exitBtn.setPosition(WIDTH / 2 - 10 * scl / 2, HEIGHT / 2 - 2 * btnH - 2 * scl);
 
         final Image logo = new Image(new Texture("logo.png"));
-        logo.setWidth(WIDTH-100);
-        logo.setHeight(WIDTH/(logo.getWidth()+200)* logo.getHeight());
-        logo.setPosition(3*scl,HEIGHT - logo.getHeight() -scl);
+        logo.setWidth(WIDTH - 100);
+        logo.setHeight(WIDTH / (logo.getWidth() + 200) * logo.getHeight());
+        logo.setPosition(3 * scl, HEIGHT - logo.getHeight() - scl);
 
-        startBtn.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y){
-                game.setScreen(new ClassicSnake(game));
-            }
-        });
+        startBtn.addListener(new ClickListener() {
+                                 @Override
+                                 public void clicked(InputEvent event, float x, float y) {
+                                     game.setScreen(new ClassicSnake(game));
+                                 }
+                             }
+        );
+
+        optionBtn.addListener(new ClickListener() {
+                                  @Override
+                                  public void clicked(InputEvent event, float x, float y) {
+                                      Gdx.app.exit();
+                                  }
+                              }
+        );
+        exitBtn.addListener(new ClickListener() {
+                                  @Override
+                                  public void clicked(InputEvent event, float x, float y) {
+                                      Gdx.app.exit();
+                                  }
+                              }
+        );
+
         stage.addActor(startBtn);
+        stage.addActor(optionBtn);
+        stage.addActor(exitBtn);
         stage.addActor(logo);
         Gdx.input.setInputProcessor(stage);
     }
